cc1d0cabf4fac5a4d850edc4f479986f5137c984
jin-co <lilijk@hanmail.net>
2023-07-06 20:54:48 +0800

    Update

--- src/app/components/selling-tax/selling-tax.component.ts
+++ src/app/components/selling-tax/selling-tax.component.ts
@@ -4,42 +4,38 @@ import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-selling-tax',
   templateUrl: './selling-tax.component.html',
-  styleUrls: ['./selling-tax.component.css']
+  styleUrls: ['./selling-tax.component.css'],
 })
 export class SellingTaxComponent {
+  taxStandard: number = 0;
+  gains: number = 0;
   revenue: number = 0;
-  totalRevenue: number = 0;
   expenses: number = 0;
-  standard: number = 0;
-  expensesRate: number = 0;
+  buyingPrice: number = 0;
+  sellingPrice: number = 0;
+  standardPrice: number = 0;
+  sellingTax: number = 0;
+  sellingTaxRate: number = 0;
+  secondarySellingTax: number = 0;
+  secondarySellingTaxRate: number = 0.05;
+  weightedTaxRate: number = 0;
   deduction: number = 0;
-  revenueTax: number = 0;
-  finalRevenueTax: number = 0;
-  regionalTax: number = 0;
-  isRegistered: boolean = false;
-
-  rentTax: number = 0;
-  depositTax: number = 0;
-  bankDepositInterestRate: number = 0;
-
-  expensesRates: { 1: number; 2: number; 3: number; 4: number; 5: number }[] = [
-    { 1: 37.4, 2: 42.6, 3: 61.6, 4: 59.2, 5: 43.4 },
-    { 1: 15.2, 2: 13.1, 3: 17.9, 4: 19, 5: 3.8 },
-  ];
+  longTermRetentionDeduction: number = 0;
+  longTermRetentionDeductionRate: number = 0;
+  longTermDwellingDeductionRate: number = 0;
+  generalDeduction: number = 2500000;
 
   form!: FormGroup;
   constructor() {
     this.form = new FormGroup({
-      rent: new FormControl(null),
-      deposit: new FormControl(null),
+      buyingPrice: new FormControl(null),
       expenses: new FormControl(null),
-      revenueDeduction: new FormControl(null),
-      isIntegratedOrSeparate: new FormControl('integrated'),
-      registered: new FormControl(false),
-      otherRevenue: new FormControl(null),
+      sellingPrice: new FormControl(null),
+      standardPrice: new FormControl(null),
       multiHome: new FormControl(0),
-      expensesRateChart: new FormControl(0),
-      lastRevenue: new FormControl(0),
+      holdingPeriod: new FormControl(0),
+      livingPeriod: new FormControl(0),
+      isRegulatedArea: new FormControl(false),
     });
   }
 
@@ -50,100 +46,164 @@ export class SellingTaxComponent {
   }
 
   calculateTax() {
-    this.expenses = +this.form.value.expenses;
+    //양도차익
+    this.gains =
+      +this.form.value.sellingPrice -
+      +this.form.value.buyingPrice -
+      +this.form.value.expenses;
 
-    //임대 경비율 계산
-    if (
-      +this.form.value.lastRevenue > 24000000 ||
-      +this.form.value.rent > 75000000
-    ) {
-      //기준
-      this.expensesRates[1][1];
-    } else {
-      //단순 소득 = 수입 - 수입 * 단순경비율
-      this.expenses =
-        this.form.value.rent - this.form.value.rent * +this.expensesRates[0][1];
-    }
-  }
-
-  onStandardChange() {
-    //간주임대료 계산식
-    this.depositTax =
-      (((+this.form.value.deposit - 300000000) * 0.6 * 1) / 365) *
-      this.bankDepositInterestRate;
-
-    this.isRegistered = !this.isRegistered;
-    if (this.form.value.isIntegratedOrSeparate == 'separated') {
-      if (this.isRegistered) {
-        this.expensesRate = 0.6;
-        this.deduction = 4000000;
-      } else {
-        this.expensesRate = 0.5;
-        this.deduction = 2000000;
+    //장기보유특별공제율 (제외대상: 미등기, 다주택자 조정지역, 국외, 조합원입주권)
+    if (this.form.value.multiHome == 1) {
+      //1세대 1주택
+      if (this.form.value.holdingPeriod >= 3) {
+        if (this.form.value.holdingPeriod < 4) {
+          this.longTermRetentionDeductionRate = 0.12;
+        } else if (this.form.value.holdingPeriod < 5) {
+          this.longTermRetentionDeductionRate = 0.16;
+        } else if (this.form.value.holdingPeriod < 6) {
+          this.longTermRetentionDeductionRate = 0.2;
+        } else if (this.form.value.holdingPeriod < 7) {
+          this.longTermRetentionDeductionRate = 0.24;
+        } else if (this.form.value.holdingPeriod < 8) {
+          this.longTermRetentionDeductionRate = 0.28;
+        } else if (this.form.value.holdingPeriod < 9) {
+          this.longTermRetentionDeductionRate = 0.32;
+        } else if (this.form.value.holdingPeriod < 10) {
+          this.longTermRetentionDeductionRate = 0.36;
+        } else {
+          this.longTermRetentionDeductionRate = 0.4;
+        }
       }
 
-      //임대소득(임대수입 - 필요경비)
-      this.revenue =
-        +this.form.value.rent +
-        +this.form.value.deposit -
-        this.expenses * this.expensesRate;
-
-      //과세표준(종합소득 - 소득공제)
-      this.standard = this.revenue - this.deduction;
-
-      //산출세액(과세표준 * 세율)
-      this.revenueTax = this.standard * 0.14;
-
-      //결정세액(산출세액 - 소형주택임대사업자 감면)
-      this.finalRevenueTax = this.revenueTax;
-
-      //지방소득세(종합소득세 * 10%)
-      this.regionalTax = this.revenueTax * 0.1;
-    } else {
-      if (this.standard <= 12000000) {
-        this.expensesRate = 0.06;
-        this.deduction = 0;
-      } else if (this.standard <= 46000000) {
-        this.expensesRate = 0.15;
-        this.deduction = 1080000;
-      } else if (this.standard <= 88000000) {
-        this.expensesRate = 0.24;
-        this.deduction = 5220000;
-      } else if (this.standard <= 150000000) {
-        this.expensesRate = 0.35;
-        this.deduction = 14900000;
-      } else if (this.standard <= 300000000) {
-        this.expensesRate = 0.38;
-        this.deduction = 19400000;
-      } else if (this.standard <= 500000000) {
-        this.expensesRate = 0.4;
-        this.deduction = 25400000;
-      } else if (this.standard <= 1000000000) {
-        this.expensesRate = 0.42;
-        this.deduction = 35400000;
-      } else {
-        this.expensesRate = 0.45;
-        this.deduction = 65400000;
+      if (this.form.value.livingPeriod >= 2) {
+        if (this.form.value.livingPeriod < 3) {
+          this.longTermDwellingDeductionRate = 0.08;
+        } else if (this.form.value.livingPeriod < 4) {
+          this.longTermDwellingDeductionRate = 0.16;
+        } else if (this.form.value.livingPeriod < 5) {
+          this.longTermDwellingDeductionRate = 0.16;
+        } else if (this.form.value.livingPeriod < 6) {
+          this.longTermDwellingDeductionRate = 0.2;
+        } else if (this.form.value.livingPeriod < 7) {
+          this.longTermDwellingDeductionRate = 0.24;
+        } else if (this.form.value.livingPeriod < 8) {
+          this.longTermDwellingDeductionRate = 0.28;
+        } else if (this.form.value.livingPeriod < 9) {
+          this.longTermDwellingDeductionRate = 0.32;
+        } else if (this.form.value.livingPeriod < 10) {
+          this.longTermDwellingDeductionRate = 0.36;
+        } else {
+          this.longTermDwellingDeductionRate = 0.4;
+        }
       }
-
-      //임대소득(임대수입 - 필요경비)
-      this.revenue =
-        +this.form.value.rent + +this.form.value.deposit - this.expenses;
-
-      //종합소득(임대소득 + 종합과세대상 다른소득)
-      this.totalRevenue = this.revenue + +this.form.value.otherRevenue;
-
-      //과세표준(종합소득 - 소득공제)
-      this.standard = this.revenue - +this.form.value.revenueDeduction;
-
-      //산출세액(과세표준 * 세율)
-      this.revenueTax = this.standard * 0.15;
-
-      //결정세액(산출세액 - 공제, 감면)
-      this.finalRevenueTax = this.revenueTax;
-
-      //지방소득세(종합소득세 * 10%)
-      this.regionalTax = this.revenueTax * 0.1;
+    } else {
+      if (!this.form.value.isRegulatedArea) {
+        this.longTermDwellingDeductionRate = 0;
+        if (this.form.value.holdingPeriod >= 3) {
+          if (this.form.value.holdingPeriod < 4) {
+            this.longTermRetentionDeductionRate = 0.06;
+          } else if (this.form.value.holdingPeriod < 5) {
+            this.longTermRetentionDeductionRate = 0.08;
+          } else if (this.form.value.holdingPeriod < 6) {
+            this.longTermRetentionDeductionRate = 0.1;
+          } else if (this.form.value.holdingPeriod < 7) {
+            this.longTermRetentionDeductionRate = 0.12;
+          } else if (this.form.value.holdingPeriod < 8) {
+            this.longTermRetentionDeductionRate = 0.14;
+          } else if (this.form.value.holdingPeriod < 9) {
+            this.longTermRetentionDeductionRate = 0.16;
+          } else if (this.form.value.holdingPeriod < 10) {
+            this.longTermRetentionDeductionRate = 0.18;
+          } else if (this.form.value.holdingPeriod < 11) {
+            this.longTermRetentionDeductionRate = 0.2;
+          } else if (this.form.value.holdingPeriod < 12) {
+            this.longTermRetentionDeductionRate = 0.22;
+          } else if (this.form.value.holdingPeriod < 13) {
+            this.longTermRetentionDeductionRate = 0.24;
+          } else if (this.form.value.holdingPeriod < 14) {
+            this.longTermRetentionDeductionRate = 0.26;
+          } else if (this.form.value.holdingPeriod < 15) {
+            this.longTermRetentionDeductionRate = 0.28;
+          } else {
+            this.longTermRetentionDeductionRate = 0.3;
+          }
+        }
+      }
     }
+
+    //장기보유특별공제 = [양도가액 - 취득가액] * 공제율
+    this.longTermRetentionDeduction =
+      (this.sellingPrice - this.buyingPrice) *
+      (this.longTermRetentionDeductionRate +
+        this.longTermDwellingDeductionRate);
+
+    //양도소득
+    this.revenue = this.gains - this.longTermRetentionDeduction;
+
+    //과세표준
+    this.taxStandard = this.revenue - this.generalDeduction;
+
+    //세율
+    if (this.taxStandard <= 12000000) {
+      this.sellingTaxRate = 0.06;
+      this.deduction = 0;
+    } else if (this.taxStandard <= 46000000) {
+      this.sellingTaxRate = 0.15;
+      this.deduction = 1080000;
+    } else if (this.taxStandard <= 88000000) {
+      this.sellingTaxRate = 0.24;
+      this.deduction = 5220000;
+    } else if (this.taxStandard <= 150000000) {
+      this.sellingTaxRate = 0.35;
+      this.deduction = 14900000;
+    } else if (this.taxStandard <= 300000000) {
+      this.sellingTaxRate = 0.38;
+      this.deduction = 19400000;
+    } else if (this.taxStandard <= 500000000) {
+      this.sellingTaxRate = 0.4;
+      this.deduction = 25400000;
+    } else if (this.taxStandard <= 1000000000) {
+      this.sellingTaxRate = 0.42;
+      this.deduction = 35400000;
+    } else {
+      this.sellingTaxRate = 0.45;
+      this.deduction = 65400000;
+    }
+
+    //다주택 중과세
+    if (this.form.value.isRegulatedArea) {
+      if (this.form.value.multiHome <= 2) {
+        this.sellingTaxRate += 0.2;
+      } else {
+        this.sellingTaxRate += 0.3;
+      }
+    }
+
+    //보유기간 중과세
+    if (this.form.value.holdingPeriod < 1) {
+      this.sellingTaxRate = 0.7;
+
+      if (this.form.value.standardPrice > 1200000000) {
+        //환산취득가산액 for ravish house
+        this.secondarySellingTax =
+          this.sellingPrice *
+          (this.buyingPrice / this.standardPrice) *
+          this.secondarySellingTaxRate;
+      }
+    } else if (this.form.value.holdingPeriod >= 2) {
+      this.sellingTaxRate = 0.6;
+
+      if (this.form.value.holdingPeriod < 5) {
+        //환산취득가산액
+        this.secondarySellingTax =
+          this.sellingPrice *
+          (this.buyingPrice / this.standardPrice) *
+          this.secondarySellingTaxRate;
+      }
+    }
+
+    this.sellingTax =
+      (this.taxStandard - this.deduction) * this.sellingTaxRate +
+      this.secondarySellingTax;
   }
 }
