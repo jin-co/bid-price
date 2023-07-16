2526e5d562e13670f4c9813a669bbc5b22de37f6
jin-co <lilijk@hanmail.net>
2023-07-16 12:07:55 +0800

    Added output comp

--- src/app/components/shared/output/output.component.ts
+++ src/app/components/shared/output/output.component.ts
@@ -7,18 +7,15 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./output.component.css'],
 })
 export class OutputComponent {
+  //common: options -> 1. y-n, 2. currency-with-rate, 3. currency 
   @Input() outputType: string = '';
   @Input() title: string = '';
-  
-  @Input() isYes: boolean = false;
   @Input() description: string = '';
+  
+  //for the option 1
+  @Input() isYes: boolean = false;
 
+  //for the option 2 and 3
   @Input() output: number = 0;
-  @Input() rate: number = 0;
-
-  constructor(public currencyPipe:CurrencyPipe) {}
-
-  selectCurrency() {
-    this.currencyPipe
-  }
+  @Input() rate: number = 0;  
 }
